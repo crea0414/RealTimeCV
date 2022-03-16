5713cbb5620c2406666afd8b07eefe9f38df6658
crea0414 <creacyhuang@gmail.com>
2022-03-16 11:50:22 +0800

    add control button to play reallity robot

--- RealTimeCV/RealityViewController.swift
+++ RealTimeCV/RealityViewController.swift
@@ -5,6 +5,9 @@
 //  Created by 吳佳穎 on 2022/3/11.
 //
 
+// How to run Object Capture App?
+// https://developer.apple.com/forums/thread/681874?answerId=678125022#678125022
+
 import UIKit
 import ARKit
 import RealityKit
@@ -12,20 +15,53 @@ import RealityKit
 class RealityViewController: UIViewController {
     var sceneFlag = true
     var mainAnchorID: UUID?
+    var box: RemoteRobot2.Box?
     @IBOutlet weak var realityView: ARView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        realityView.debugOptions.insert(.showFeaturePoints)
+//        realityView.debugOptions.insert(.showFeaturePoints)
         if #available(iOS 13.4, *) {
-            realityView.debugOptions.insert(.showSceneUnderstanding)
+//            realityView.debugOptions.insert(.showSceneUnderstanding)
         } else {
             // Fallback on earlier versions
         }
 //        realityView.debugOptions.insert(.showWorldOrigin)
         
     }
+    @IBAction func upPressed(_ sender: UIButton) {
+        guard let box = self.box else {return}
+        box.notifications.robotForward.post()
+    }
+    @IBAction func downPressed(_ sender: UIButton) {
+        guard let box = self.box else {return}
+        box.notifications.robotBackward.post()
+    }
+    @IBAction func rightPressed(_ sender: UIButton) {
+        guard let box = self.box else {return}
+        box.notifications.robotRight.post()
+    }
+    @IBAction func leftPressed(_ sender: UIButton) {
+        guard let box = self.box else {return}
+        box.notifications.robotLeft.post()
+    }
+    @IBAction func jumoPressed(_ sender: UIButton) {
+        guard let box = self.box else {return}
+        box.notifications.robotFly.post()
+    }
+    @IBAction func homePressed(_ sender: UIButton) {
+        guard let box = self.box else {return}
+        box.notifications.robotHome.post()
+    }
+    @IBAction func uturnPressed(_ sender: UIButton) {
+        guard let box = self.box else {return}
+        box.notifications.robotUturn.post()
+    }
+    
+    
+    
+    
     
     //https://stackoverflow.com/questions/60294367/how-to-use-raycast-methods-in-realitykit
     @IBAction func tapPressed(_ sender: UITapGestureRecognizer) {
@@ -34,19 +70,16 @@ class RealityViewController: UIViewController {
             let position = sender.location(in: realityView)
 //            guard let query = realityView.makeRaycastQuery(from: position, allowing: .estimatedPlane, alignment: .any)else {return}
 //            let results = realityView.session.raycast(query)
-            
             let results = realityView.raycast(from: position, allowing: .estimatedPlane, alignment: .horizontal)
-            print("Results: \(results.count)")
             guard let rayCast = results.first else {print("No surface found"); return}
             DispatchQueue.global(qos: .background).sync {
                 let anchor = AnchorEntity(world: rayCast.worldTransform)
                 guard let scene = try? RemoteRobot2.loadBox() else {print("Loading Scene fial!"); return}
 //                guard let scene = try?  MyScene.loadBox() else {print("Loading Scene fial!"); return}
-                anchor.addChild(scene)
-                if let anchorID = anchor.anchorIdentifier{
-                    self.mainAnchorID = anchorID
-                    print(String("anchor: \(self.mainAnchorID)"))
-                }
+
+                self.box = scene
+                anchor.addChild(self.box!)
+                
                 self.realityView.scene.anchors.append(anchor)
             }
             sceneFlag = false
