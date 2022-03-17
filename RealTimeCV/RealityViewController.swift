cebfd6a166650d2b330b984aa2fa141ed68aca67
crea0414 <creacyhuang@gmail.com>
2022-03-17 14:01:09 +0800

    create missile feature in reality for more interaction and show ar statistic on the screen to monitor ar redering condition

--- RealTimeCV/RealityViewController.swift
+++ RealTimeCV/RealityViewController.swift
@@ -14,16 +14,26 @@ import RealityKit
 
 class RealityViewController: UIViewController {
     var sceneFlag = true
+    var missileFlag = false
     var mainAnchorID: UUID?
     var box: RemoteRobot2.Box?
+//    lazy var missiel: Missile._Missile? = {
+//        do {
+//            return try Missile.load_Missile()
+//        }catch{
+//            fatalError("Loading missile fail!")
+//        }
+//    }()
+    var missiel: Missile._Missile?
     @IBOutlet weak var realityView: ARView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //        realityView.debugOptions.insert(.showFeaturePoints)
+            realityView.debugOptions.insert(.showStatistics)
         if #available(iOS 13.4, *) {
-//            realityView.debugOptions.insert(.showSceneUnderstanding)
+            realityView.debugOptions.insert(.showSceneUnderstanding)
         } else {
             // Fallback on earlier versions
         }
@@ -59,6 +69,14 @@ class RealityViewController: UIViewController {
         box.notifications.robotUturn.post()
     }
     
+    @IBAction func firePressed(_ sender: UIButton) {
+        if self.missileFlag {
+            sender.tintColor = UIColor(red: 0, green: 0.0, blue: 0.0, alpha: 0.0)
+        }else{
+            sender.tintColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        }
+        self.missileFlag = !self.missileFlag
+    }
     
     
     
@@ -68,37 +86,55 @@ class RealityViewController: UIViewController {
         if sceneFlag {
             
             let position = sender.location(in: realityView)
-//            guard let query = realityView.makeRaycastQuery(from: position, allowing: .estimatedPlane, alignment: .any)else {return}
-//            let results = realityView.session.raycast(query)
-            let results = realityView.raycast(from: position, allowing: .estimatedPlane, alignment: .horizontal)
-            guard let rayCast = results.first else {print("No surface found"); return}
+            guard let rayCast = getRayCastResult(position) else {return}
+
             DispatchQueue.global(qos: .background).sync {
                 let anchor = AnchorEntity(world: rayCast.worldTransform)
                 guard let scene = try? RemoteRobot2.loadBox() else {print("Loading Scene fial!"); return}
-//                guard let scene = try?  MyScene.loadBox() else {print("Loading Scene fial!"); return}
-
                 self.box = scene
                 anchor.addChild(self.box!)
-                
                 self.realityView.scene.anchors.append(anchor)
             }
             sceneFlag = false
         }else {
             let tapLocation = sender.location(in: realityView)
-
                     if let hitEntity = realityView.entity(
                         at: tapLocation
                     ) {
                         print("touched")
                         print("hitEntityName: \(hitEntity.name)")
-                        // touched !
-
-//                        return ;
                     }
+        }
+        
+        if self.missileFlag{
+            let position = sender.location(in: realityView)
+            let results = realityView.raycast(from: position, allowing: .estimatedPlane, alignment: .any)
+            guard let rayCast = results.first else {print("No surface found"); return}
+            DispatchQueue.global(qos: .background).sync {
+                let newAnchor = AnchorEntity(world: rayCast.worldTransform)
+                guard let missiel = try? Missile.load_Missile() else {print("Loading missile fail!"); return}
+                self.missiel = missiel
+                newAnchor.addChild(self.missiel!)
+                if self.realityView.scene.anchors.count < 2 {
+                    self.realityView.scene.anchors.append(newAnchor)
+                }else{
+                    self.realityView.scene.anchors.remove(at: self.realityView.scene.anchors.count-1)
+                    self.realityView.scene.anchors.append(newAnchor)
+                }
+                guard let m = self.missiel else {return}
+                m.notifications.fire.post()
+                
+            }
+            print("After add new anchor -> anchor_nums:\(self.realityView.scene.anchors.count)")
         }
         
     }
     
+    private func getRayCastResult(_ position: CGPoint) -> ARRaycastResult?{
+        let results = realityView.raycast(from: position, allowing: .estimatedPlane, alignment: .horizontal)
+        guard let rayCast = results.first else {print("No surface found"); return nil}
+        return rayCast
+    }
     
     
     @IBAction func backPressed(_ sender: UIButton) {
